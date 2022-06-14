4ab11dd35b71421d84728d86e4a83a4a05517bae
Ben Huggins <benshuggins@gmail.com>
2022-06-14 16:09:37 -0700

    added images and google interview problem 2

---
+++ Swift_Problems.playground/Pages/NumberIslands - Google Interview.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Number Islands: Problem is in resources folder for this playgrounds page.
+
+// Return the number of islands, which are 1's completely surrounded by water.
+
+
+
